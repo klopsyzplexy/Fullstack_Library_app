73df53ba54076cf9db57ff1c22308c29eea7bbf7
klopsyzplexy <dominik.bednarski4@gmail.com>
2024-04-26 18:26:04 +0200

    project

---
+++ libbapp/books/views.py
@@ -0,0 +1,10 @@
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import viewsets
+from .models import Book
+from .serializers import BookSerializer
+
+class BookViewSet(viewsets.ModelViewSet):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['genre', 'author']
